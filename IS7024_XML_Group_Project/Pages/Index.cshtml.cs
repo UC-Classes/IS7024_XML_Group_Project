d26da21881a86024cb4d8ec8467942a04904fa05
Jacob Herdeman <jakeherdeman@yahoo.com>
2019-11-16 00:11:54 -0500

    Adding Candy Class

--- IS7024_XML_Group_Project/Pages/Index.cshtml.cs
+++ IS7024_XML_Group_Project/Pages/Index.cshtml.cs
@@ -1,10 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
+using QuickTypeCandy;
 
 namespace IS7024_XML_Group_Project.Pages
 {
@@ -19,7 +23,13 @@ namespace IS7024_XML_Group_Project.Pages
 
         public void OnGet()
         {
-
+            //using QuickTypecandy;
+            using (var webClient = new WebClient())
+            {
+                string jsonString = webClient.DownloadString("https://raw.githubusercontent.com/UC-Classes/IS7024_XML_Group_Project/master/candyfile.txt");
+                var candy = Candy.FromJson(jsonString);
+                ViewData["Candy"] = candy;
+            }
         }
     }
 }
