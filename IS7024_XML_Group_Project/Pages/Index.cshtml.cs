a9f8ca2844027e10750a6b2d9371bf555ff1c068
Jacob Herdeman <jakeherdeman@yahoo.com>
2019-11-16 01:27:41 -0500

    Added weather data

--- IS7024_XML_Group_Project/Pages/Index.cshtml.cs
+++ IS7024_XML_Group_Project/Pages/Index.cshtml.cs
@@ -9,6 +9,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
 using QuickTypeCandy;
+using QuickTypeWeather;
 
 namespace IS7024_XML_Group_Project.Pages
 {
@@ -23,13 +24,45 @@ namespace IS7024_XML_Group_Project.Pages
 
         public void OnGet()
         {
+           double temp = 0;
+
+
             //using QuickTypecandy;
             using (var webClient = new WebClient())
             {
                 string jsonString = webClient.DownloadString("https://raw.githubusercontent.com/UC-Classes/IS7024_XML_Group_Project/master/candyfile.txt");
                 var candy = Candy.FromJson(jsonString);
                 ViewData["Candy"] = candy;
+            
+
+            // get our weather data and key
+            string weatherAPIKey = System.IO.File.ReadAllText("WeatherAPIKey.txt");
+            string weatherData = webClient.DownloadString("https://api.weatherbit.io/v2.0/current?&city=Cincinnati&country=USA&key=" + weatherAPIKey);
+
+            // parse to objects
+            QuickTypeWeather.Weather weather = QuickTypeWeather.Weather.FromJson(weatherData);
+            QuickTypeWeather.Datum[] allWeatherData = weather.Data;
+
+
+                foreach (QuickTypeWeather.Datum datum in allWeatherData)
+                {
+                    temp = datum.Temp;
+                }
+
+                // if statement to show message if it's raining or not
+                if (temp < 5)
+                {
+                    ViewData["WeatherMessage"] = "Bundle up, it's cold!";
+
+                }
+                else
+                {
+                    ViewData["WeatherMessage"] = "Temperature is great!";
+
+                }
+
             }
+
         }
     }
 }
