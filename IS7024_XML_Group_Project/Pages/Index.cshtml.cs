f79150341d3c56e1711dcdd7044ed284fde58e04
menonsn <54641774+menonsn@users.noreply.github.com>
2019-11-17 17:06:18 -0500

    Code Review for Halloween Trick or Treat Project.

--- IS7024_XML_Group_Project/Pages/Index.cshtml.cs
+++ IS7024_XML_Group_Project/Pages/Index.cshtml.cs
@@ -26,29 +26,28 @@ namespace IS7024_XML_Group_Project.Pages
         {
            double temp = 0;
 
-
-
-            //pull in JSON stream for candy
-            using (var webClient = new WebClient())
-            {
-                string jsonString = webClient.DownloadString("https://raw.githubusercontent.com/UC-Classes/IS7024_XML_Group_Project/master/candyfile.txt");
-                var candy = Candy.FromJson(jsonString);
-                ViewData["Candy"] = candy;
+           //pull in JSON stream for candy
+           string jsonString = DownloadData("https://raw.githubusercontent.com/UC-Classes/IS7024_XML_Group_Project/master/candyfile.txt");
+           var candydata = Candy.FromJson(jsonString);
+           ViewData["Candy"] = candydata;
             
 
             // get our weather data and key
             string weatherAPIKey = System.IO.File.ReadAllText("WeatherAPIKey.txt");
-            string weatherData = webClient.DownloadString("https://api.weatherbit.io/v2.0/current?&city=Cincinnati&country=USA&key=" + weatherAPIKey);
+            string weatherData = DownloadData("https://api.weatherbit.io/v2.0/current?&city=Cincinnati&country=USA&key=" + weatherAPIKey);
 
             // parse to objects
             QuickTypeWeather.Weather weather = QuickTypeWeather.Weather.FromJson(weatherData);
             QuickTypeWeather.Datum[] allWeatherData = weather.Data;
 
             //check temperature from weather data
-                foreach (QuickTypeWeather.Datum datum in allWeatherData)
-                {
-                    temp = datum.Temp;
-                }
+            //   foreach (QuickTypeWeather.Datum datum in allWeatherData)
+            //   {
+            //         temp = datum.Temp;
+            //    }
+
+            //check temperature from weather data
+            temp = allWeatherData[0].Temp;
 
                 // if statement to show message temperature is below 5*C
                 if (temp < 5)
@@ -62,8 +61,16 @@ namespace IS7024_XML_Group_Project.Pages
 
                 }
 
-            }
+        }
 
+        public string DownloadData(string jsonendpoint)
+        {
+            string rawData = "";
+            using (WebClient webClient = new WebClient())
+            {
+                rawData = webClient.DownloadString(jsonendpoint);
+            }
+            return rawData;
         }
     }
 }
