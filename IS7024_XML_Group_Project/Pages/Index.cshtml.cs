f9a00c0b5769d268c1ad2d1c36489652e28e9428
divetools <41244877+divetools@users.noreply.github.com>
2019-12-07 08:39:49 -0500

    Image Fixed

--- IS7024_XML_Group_Project/Pages/Index.cshtml.cs
+++ IS7024_XML_Group_Project/Pages/Index.cshtml.cs
@@ -15,12 +15,12 @@ namespace IS7024_XML_Group_Project.Pages
 {
     public class IndexModel : PageModel
     {
-        private readonly ILogger<IndexModel> _logger;
+        //private readonly ILogger<IndexModel> _logger;
 
-        public IndexModel(ILogger<IndexModel> logger)
-        {
-            _logger = logger;
-        }
+        //public IndexModel(ILogger<IndexModel> logger)
+        //{
+        //    _logger = logger;
+        //}
 
         public void OnGet()
         {
